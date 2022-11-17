d9a143075991bb9de88dbe38ce288b708aa9af05
RamyaGangadharan <67957110+RamyaGangadharan@users.noreply.github.com>
2022-11-17 21:51:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ test= """<!DOCTYPE html>
   	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"></script>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-	<title>PDS Desktop Tests</title>
+	<title>Terraform Project</title>
 </head>
 <body>
 	<div class="container my-5">
